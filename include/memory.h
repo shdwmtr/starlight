9a8a06960027b17c2b03d081c9d812c6be8fd221
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-04-03 18:54:03 -0300

    refactor: Restructure and copy right source code.

--- include/memory.h
+++ include/memory.h
@@ -1,6 +1,35 @@
+/**
+ * ==================================================
+ *   _____ _ _ _             _                     
+ *  |     |_| | |___ ___ ___|_|_ _ _____           
+ *  | | | | | | | -_|   |   | | | |     |          
+ *  |_|_|_|_|_|_|___|_|_|_|_|_|___|_|_|_|          
+ * 
+ * ==================================================
+ * 
+ * Copyright (c) 2025 Project Millennium
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #pragma once
 
-// array size is 149972
 static const unsigned char GeistVaraible[]  = {
   0x00, 0x01, 0x00, 0x00, 0x00, 0x15, 0x01, 0x00, 0x00, 0x04, 0x00, 0x50, 0x47, 0x44, 0x45, 0x46, 
   0xed, 0xd0, 0x26, 0x9e, 0x00, 0x00, 0xdf, 0x3c, 0x00, 0x00, 0x04, 0xaa, 0x47, 0x50, 0x4f, 0x53, 
@@ -9378,8 +9407,6 @@ static const unsigned char GeistVaraible[]  = {
   0x43, 0x79, 0x72, 0x00
 };
 
-
-// array size is 113904
 static const unsigned char Geist_Bold[]  = {
   0x00, 0x01, 0x00, 0x00, 0x00, 0x12, 0x01, 0x00, 0x00, 0x04, 0x00, 0x20, 0x47, 0x44, 0x45, 0x46, 
   0x46, 0x6e, 0x40, 0x0e, 0x00, 0x01, 0x67, 0x28, 0x00, 0x00, 0x01, 0x20, 0x47, 0x50, 0x4f, 0x53, 
--- include/task_scheduler.h
+++ include/task_scheduler.h
@@ -1,34 +1,48 @@
-#ifndef TASK_SCHEDULER_H
-#define TASK_SCHEDULER_H
+/**
+ * ==================================================
+ *   _____ _ _ _             _                     
+ *  |     |_| | |___ ___ ___|_|_ _ _____           
+ *  | | | | | | | -_|   |   | | | |     |          
+ *  |_|_|_|_|_|_|___|_|_|_|_|_|___|_|_|_|          
+ * 
+ * ==================================================
+ * 
+ * Copyright (c) 2025 Project Millennium
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
+#pragma once
 #include <vector>
 #include <functional>
 #include <memory>
 
 class TaskScheduler {
 public:
-    // Define task type: function that takes a unique pointer to a double for progress tracking
     using Task = std::function<void(std::unique_ptr<double>&)>;
 
-    // Constructor
     TaskScheduler();
-    
-    // Add a single task to the scheduler
     void addTask(Task task);
-
-    // Add multiple tasks at once
     void addTasks(const std::vector<Task>& newTasks);
-
-    // Get the current overall progress (0.0 to 1.0)
     double getProgress() const;
-
-    // Execute all tasks in sequence
     void run();
-
-    // Get number of tasks
     size_t getTaskCount() const;
-
-    // Get current task index (0-based)
     size_t getCurrentTaskIndex() const;
 
 private:
@@ -37,5 +51,3 @@ private:
     size_t currentTaskIndex;
     std::unique_ptr<double> currentTaskProgress;
 };
-
-#endif // TASK_SCHEDULER_H
